28423420e25e5a8ea12843e166376c6e53e15cfb
Charan-Patnaik <avscharan.patnaik@gmail.com>
2023-08-02 01:21:54 -0400

    push from wsl --version 2

--- first.js
+++ first.js
@@ -1,5 +1,2 @@
 const fs = require('fs')
 fs.writeFileSync('output.txt', ' hello from node.js')
-
-git config --global user.email "avscharan.patnaik@gmail.com"
-git config --global user.name "Charan-Patnaik"
